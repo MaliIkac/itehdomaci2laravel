550c118cb47cf110b2e6ce9edca0e222b309a4ab
Iva <ivaa99petkovic@gmail.com>
2021-12-19 17:03:19 +0100

    FINISH :)

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ClipController;
+use App\Http\Controllers\LikeController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +18,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'login']);
+Route::get('/clips', [ClipController::class, 'index']);
+Route::get('/categories', [CategoryController::class, 'index']);
+Route::get('/likes', [LikeController::class, 'index']);
+
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::post('/logout', [UserController::class, 'logout']);
+
+    Route::post('/clips', [ClipController::class, 'store']);
+    Route::delete('/clips/{clip}', [ClipController::class, 'destroy']);
+
+    Route::post('/likes', [LikeController::class, 'store']);
+    Route::delete('/likes/{like}', [LikeController::class, 'destroy']);
 });
